b481dfb43f859ef5a34922b042c8262744009cb2
murilo <murilorockstar492@gmail.com>
2026-01-20 09:27:17 -0300

    fix: add context for control as example

--- internal/repo/clientrepo.go
+++ internal/repo/clientrepo.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/projeto-crm-2026/crm-services/internal/domain/entity"
 	"gorm.io/gorm"
@@ -23,8 +24,11 @@ func NewClientRepo(db *gorm.DB) ClientRepo {
 func (r *clientRepo) GetAll(ctx context.Context) ([]entity.Client, int64, error) {
 	var clients []entity.Client
 	var total int64
-	query := r.db.Select("name")
-	query.Find(&clients)
+
+	if err := r.db.WithContext(ctx).
+		Find(&clients).Error; err != nil {
+		return nil, 0, fmt.Errorf("failed to get clients: %w", err)
+	}
 
 	return clients, total, nil
 }
